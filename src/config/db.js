2b7f44c3076c129a3c705e45f3688f6025d08351
Seiha <naingseiha@gmail.com>
2025-06-20 21:11:13 +0200

    Update port

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
+  const uri = process.env.MONGO_URI;
 
-    console.log("MongoDB connected successfully");
-  } catch (error) {
-    console.error("MongoDB connection failed:", error.message);
-    process.exit(1); // Exit the process with failure
+  if (!uri) {
+    throw new Error("MONGO_URI is not defined in environment variables");
   }
+
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 };
